564bca920a72e552e26cd122a37bd7abd89dae9c
Thomas Rasshofer <me@thomasrasshofer.com>
2014-10-16 16:19:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# TinyPNG CLI
+
+> Handy command line tool for shrinking PNG images using the TinyPNG API
+
+## Installation
+
+	npm install -g tinypng-cli
+
+## Preamble
+
+To use TinyPNG CLI, you need an API key for TinyPNG. You can get one at [https://tinypng.com/developers](https://tinypng.com/developers).
+
+## Usage
+
+TinyPNG CLI allows you to provide your API key in two different ways. The more convenient one is to save the API key into a file called `.tinypng` within your home directory. The other way is to provide it as an option while running the CLI.
+
+	tinypng demo.png -k E99a18c4f8cb3EL5f2l08u368_922e03
+
+To shrink all PNG images within the current directory and its subdirectories, you may run one of the following commands—both do exactly the same.
+
+	tinypng
+	tinypng .
+
+***
+	
+To shrink all PNG images within a specific directory (`assets/img` in this example) and its subdirectories, you may run the following command.
+
+	tinypng assets/img
+
+You may also provide multiple directories.
+
+	tinypng assets/img1 assets/img2
+	
+***
+	
+To shrink a single PNG image (`assets/img/demo.png` in this example), you may run the following command.
+
+	tinypng assets/img/demo.png
+
+You may also provide multiple single PNG images.
+
+	tinypng assets/img/demo1.png assets/img/demo2.png
+
+That's it. Pretty easy, huh?
+
+## Changelog
+
+* 0.0.1
+	* Initial version
+
+## TODO
+
+- Documentation
+- Tests
+
+## License
+
+Copyright (c) 2014 [websperts](http://websperts.com/)  
+Licensed under the MIT license.
+
+See LICENSE for more info.
---
+++ tinypng-cli.js
@@ -0,0 +1,125 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var request = require('request');
+var minimatch = require('minimatch');
+var glob = require('glob');
+var uniq = require('array-uniq');
+var chalk = require('chalk');
+var pretty = require('prettysize');
+
+var argv = require('minimist')(process.argv.slice(2));
+var home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
+var version = '0.0.1';
+
+if (argv.v || argv.version) {
+
+    console.log(version);
+
+} else if (argv.h || argv.help) {
+
+    console.log(
+        'Usage\n' +
+        '  tinypng <path>\n' +
+        '\n' +
+        'Example\n' +
+        '  tinypng .\n' +
+        '  tinypng assets/img\n' +
+        '  tinypng assets/img/test.png\n' +
+        '\n' +
+        'Options\n' +
+        '  -k, --key         Provide an API key\n' +
+        '  -v, --version     Show installed version\n' +
+        '  -h, --help        Show help'
+    );
+
+} else {
+
+    console.log(chalk.underline.bold('TinyPNG CLI'));
+    console.log('v' + version + '\n');
+
+    var files = argv._.length ? argv._ : ['.'];
+
+    var key = '';
+
+    if (argv.k || argv.key) {
+        key = typeof (argv.k || argv.key) === 'string' ? (argv.k || argv.key).trim() : '';
+    } else if (fs.existsSync(home + '/.tinypng')) {
+        key = fs.readFileSync(home + '/.tinypng', 'utf8').trim();
+    }
+
+    if (key.length === 0) {
+
+        console.log(chalk.bold.red('No API key specified. You can get one at ' + chalk.underline('https://tinypng.com/developers') + '.'));
+
+    } else {
+
+        var images = [];
+
+        files.forEach(function(file) {
+            if (fs.existsSync(file)) {
+                if (fs.lstatSync(file).isDirectory()) {
+                    images = images.concat(glob.sync(file + '/**/*.png'));
+                } else if(minimatch(file, '*.png', {
+                    matchBase: true
+                })) {
+                    images.push(file);
+                }
+            }
+        });
+
+        var unique = uniq(images);
+
+        if (unique.length === 0) {
+
+            console.log(chalk.bold.red('\u2718 No PNG images found.'));
+
+        } else {
+
+            console.log(chalk.bold.green('\u2714 Found ' + unique.length + ' PNG image' + (unique.length === 1 ? '' : 's')) + '\n');
+            console.log(chalk.bold('Processing...'));
+
+            unique.forEach(function(file) {
+
+                fs.createReadStream(file).pipe(request.post('https://api.tinypng.com/shrink', {
+                    auth: {
+                        'user': 'api',
+                        'pass': key
+                    }
+                }, function (error, response, body) {
+
+                    body = JSON.parse(body);
+
+                    if (response.statusCode === 201) {
+
+                        if (body.output.size < body.input.size) {
+
+                            console.log(chalk.green('\u2714 Panda just saved you ' + chalk.bold(pretty(body.input.size - body.output.size) + ' (' + Math.round(100 - 100 / body.input.size * body.output.size) + '%)') + ' for `' + file + '`'));
+                            request.get(body.output.url).pipe(fs.createWriteStream(file));
+
+                        } else {
+
+                            console.log(chalk.yellow('\u2718 Couldn’t compress `' + file + '` any further'));
+
+                        }
+
+                    } else {
+
+                        if (body.error === 'TooManyRequests') {
+                            console.log(chalk.red('\u2718 Compression failed for `' + file + '` as your monthly limit has been exceeded'));
+                        } else if (body.error === 'Unauthorized') {
+                            console.log(chalk.red('\u2718 Compression failed for `' + file + '` as your credentials are invalid'));
+                        } else {
+                            console.log(chalk.red('\u2718 Compression failed for `' + file + '`'));
+                        }
+
+                    }
+                }));
+
+            });
+
+        }
+
+    }
+
+}
